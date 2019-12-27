bcffc54e26009823ab674bb049b8b04b88d14458
hee <heexudong@163.com>
2019-12-27 15:07:52 +0800

    super commit

--- src/test/java/com/hxd/springbootredis/SpringbootRedisApplicationTests.java
+++ src/test/java/com/hxd/springbootredis/SpringbootRedisApplicationTests.java
@@ -1,14 +1,11 @@
 package com.hxd.springbootredis;
 
 import com.hxd.springbootredis.common.utils.RedisUtil;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootRedisApplicationTests {
 
